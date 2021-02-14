f46f8d3e07d7dff4d3de63950e952a131a50e458
aj <arnarjonasson@gmail.com>
2021-02-14 19:06:38 +0100

    end of exercise1

--- Ejercicio1/ejercicio1.py
+++ Ejercicio1/ejercicio1.py
@@ -1,18 +1,19 @@
 
 
-def check_if_perfect(number):
+numbers_list = [46, 56, 112, 28, 17, 496, 23, 555, 8128, 156, 6544, 1235455]
 
-    sum = 0    
-    for i in range(1, number):
-        if number%i == 0:
-            sum +=i
+def check_if_perfect(numbers_list):
 
-    if sum < number: print('Number is defective')
-    if sum > number: print('Number is abundant')
-    if sum == number: print('Number is perfect')
+    for n in numbers_list:
+
+        sum = 0    
+        for i in range(1, n):
+            if n%i == 0:
+                sum +=i
+                
+        if sum < n: print('Number is defective')
+        if sum > n: print('Number is abundant')
+        if sum == n: print('Number is perfect')
 
 
-try:
-    check_if_perfect(number = int(input('Enter number:')))
-except ValueError:
-    print('Invalid input')
+check_if_perfect(numbers_list)
